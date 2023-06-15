5669c45a1b5c830dc4d6784c656c8ff2b52c1500
Deep Jawale <deepjawale801@gmail.com>
2023-06-15 15:38:53 +0530

    added basic media query boilerplate

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-import { ReactDOM } from "react-dom";
 import Homestyle from "./Home.module.css";
 
-import {Link, useNavigate} from 'react-router-dom';
+import { useNavigate} from 'react-router-dom';
 
 import ButtonLight from "../../components/Buttons/ButtonLight.jsx";
 import ButtonDark from "../../components/Buttons/ButtonDark.jsx";
