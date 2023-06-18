81f92661936d9fd35efa687c7c69ffdd8d1e422f
Deep Jawale <deepjawale801@gmail.com>
2023-06-18 15:57:01 +0530

    Added route to search page from home

--- src/components/Buttons/ButtonHollow.jsx
+++ src/components/Buttons/ButtonHollow.jsx
@@ -2,12 +2,24 @@ import React from "react";
 import { ReactDOM } from "react-dom";
 import ButtonHollowstyle from "./ButtonHollow.module.css";
 
-function ButtonHollow (props) {
-	// const styleObj = {};
-	return(
-			props.type === "white" ? <button style={props.style} className={ButtonHollowstyle.basicButtonwhite} onClick={props.onClick}>{props.text}</button>:
-			<button className={ButtonHollowstyle.basicButtonprimary} onClick={props.onClick}>{props.text}</button>
-	)
+function ButtonHollow(props) {
+  // const styleObj = {};
+  return props.type === "white" ? (
+    <button
+      style={props.style}
+      className={ButtonHollowstyle.basicButtonwhite}
+      onClick={props.ClickFunction}
+    >
+      {props.text}
+    </button>
+  ) : (
+    <button
+      className={ButtonHollowstyle.basicButtonprimary}
+      onClick={props.ClickFunction}
+    >
+      {props.text}
+    </button>
+  );
 }
 
 export default ButtonHollow;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Homestyle from "./Home.module.css";
 
-import { useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 import ButtonLight from "../../components/Buttons/ButtonLight.jsx";
 import ButtonDark from "../../components/Buttons/ButtonDark.jsx";
@@ -9,54 +9,83 @@ import ButtonHollow from "../../components/Buttons/ButtonHollow.jsx";
 import Menubar from "../../components/Menubar/Menubar";
 import Footer from "../../components/Footer/Footer";
 
+function Home() {
+  const [onClick, setOnClick] = useState();
+  const navigate = useNavigate();
+  const handleClicktoLogin = () => {
+    navigate("/login");
+    console.log("clicked");
+  };
+  const handleClicktoSearch = () => {
+    navigate("/search");
+    console.log("Clicked");
+  };
 
-function Home () {
-	const [onClick, setOnClick] = useState(); 
-	const navigate = useNavigate();
-	const handleClick= () =>{
-		navigate("/login");
-		console.log("clicked");
-	}
+  return (
+    <div className={Homestyle.container}>
+      <div className={Homestyle.section}>
+        <div className={Homestyle.actualsection}>
+          <div className={Homestyle.navbar}>
+            <p className={Homestyle.logotext}>MEDIC.</p>
 
-
-
-	return(
-		<div className={Homestyle.container}>
-			<div className={Homestyle.section}>
-				<div className={Homestyle.actualsection}>
-					<div className={Homestyle.navbar}>
-						<p className={Homestyle.logotext}>MEDIC.</p>
-						
-						<ButtonDark text="LOGIN" ClickFunction={handleClick} style={{fontSize: "15px", borderRadius: "20px", marginRight: "15px"}}>
-						</ButtonDark>
-						<ButtonLight children="Are you a doctor?" ClickFunction={handleClick} style={{fontSize: "15px", borderRadius: "20px", marginRight: "15px", minWidth: "max-content"}}/>
-					</div>
-					<Menubar/>
-				</div>
-				<div className={Homestyle.main}>
-					<div className={Homestyle.mainLeft}>
-						<p className={Homestyle.text1} >CONSULT ON</p>
-						<div className={Homestyle.mainLeftSecond}>
-							{/* <p style={{fontSize: "0.8rem", color: "white",maxWidth: "fit-content", margin: "0"}}>Store all your<br/> medical records in a <br/> secure vault with <br/> our decentralized<br/> solution</p> */}
-							<p className={Homestyle.text2} >ANY ISSUE</p>
-						</div>
-						<div className={Homestyle.mainLeftThird}>
-							<img src={require("../../assets/images/avatargroupimg.png")} alt="avatar" height="60rem"/>
-							<p className={Homestyle.text1} >ONLINE</p>
-						</div>
-					</div>
-					<div className={Homestyle.mainRight}>
-						<img src={require("../../assets/images/mainleftimg.png")} alt="healthpluslogo"/>
-					</div>
-				</div>
-				<div className={Homestyle.mainDown}>
-					<ButtonDark style={{fontSize: "15px"}} text="Book an Appointment"/>
-					<ButtonHollow style={{fontSize: "15px"}} type="white" text="Search for doctors"></ButtonHollow>
-				</div>
-			</div>
-			<Footer/>
-		</div>
-	)
+            <ButtonDark
+              text="LOGIN"
+              ClickFunction={handleClicktoLogin}
+              style={{
+                fontSize: "15px",
+                borderRadius: "20px",
+                marginRight: "15px",
+              }}
+            ></ButtonDark>
+            <ButtonLight
+              children="Are you a doctor?"
+              ClickFunction={handleClicktoLogin}
+              style={{
+                fontSize: "15px",
+                borderRadius: "20px",
+                marginRight: "15px",
+                minWidth: "max-content",
+              }}
+            />
+          </div>
+          <Menubar />
+        </div>
+        <div className={Homestyle.main}>
+          <div className={Homestyle.mainLeft}>
+            <p className={Homestyle.text1}>CONSULT ON</p>
+            <div className={Homestyle.mainLeftSecond}>
+              {/* <p style={{fontSize: "0.8rem", color: "white",maxWidth: "fit-content", margin: "0"}}>Store all your<br/> medical records in a <br/> secure vault with <br/> our decentralized<br/> solution</p> */}
+              <p className={Homestyle.text2}>ANY ISSUE</p>
+            </div>
+            <div className={Homestyle.mainLeftThird}>
+              <img
+                src={require("../../assets/images/avatargroupimg.png")}
+                alt="avatar"
+                height="60rem"
+              />
+              <p className={Homestyle.text1}>ONLINE</p>
+            </div>
+          </div>
+          <div className={Homestyle.mainRight}>
+            <img
+              src={require("../../assets/images/mainleftimg.png")}
+              alt="healthpluslogo"
+            />
+          </div>
+        </div>
+        <div className={Homestyle.mainDown}>
+          <ButtonDark style={{ fontSize: "15px" }} text="Book an Appointment" />
+          <ButtonHollow
+            ClickFunction={handleClicktoSearch}
+            style={{ fontSize: "15px" }}
+            type="white"
+            text="Search for doctors"
+          ></ButtonHollow>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default Home;
