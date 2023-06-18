1df4b2cf2e62e55444c956cad982ed4da6a3b262
Deep Jawale <deepjawale801@gmail.com>
2023-06-18 18:18:17 +0530

    added disabled styling of the searchfield

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -4,13 +4,14 @@ import styles from "./styles.module.css";
 export default function SearchField({ widthOfEach, options1, options2 }) {
   return (
     <div className={styles.container}>
-      <select style={{ width: `${widthOfEach}` }} className={styles.sel1}>
-        <img src="./assets/locationIcon.png" alt="" />
+      <select style={{ width: `${widthOfEach}` }} className={styles.sel1} required>
+        <option value="" hidden disabled selected>Location</option>
         {options1.map((optionText) => {
           return <option value={optionText}>{optionText}</option>;
         })}
       </select>
-      <select style={{ width: `${widthOfEach}` }} className={styles.sel2}>
+      <select style={{ width: `${widthOfEach}` }} className={styles.sel2} required>
+      <option value="" hidden disabled selected>Specialty</option>
         {options2.map((optionText) => {
           return <option value={optionText}>{optionText}</option>;
         })}
