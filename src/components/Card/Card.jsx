ee095415202b02fda8faada61b0cf3f039f71712
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-06-20 00:35:30 +0530

    API integration of nearby doctors

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,14 +6,14 @@ export default function Card({ doctor, style }) {
 	return (
 		<div className={styles.card}>
 			<div className={styles.container}>
-				<Avatar imgURL={doctor.imgURL} />
+				<Avatar imgURL={doctor.image} />
 				<div className={styles.nameBox}>
-					<p className={styles.text2}>{doctor.fullName}</p>
+					<p className={styles.text2}>{doctor.name}</p>
 				</div>
 				<div className={styles.infoBox}>
 					<p className={styles.text3}>{doctor.degree}</p>
 					<span className={styles.divider}></span>
-					<p className={styles.text4}>{doctor.specialty}</p>
+					<p className={styles.text4}>{doctor.specialities[0]}</p>
 					<span className={styles.divider}></span>
 					<p className={styles.text5}>{doctor.experience}</p>
 				</div>
