fd5f82ff3ded75019b55353ab72e3a304cd1be96
Deep Jawale <deepjawale801@gmail.com>
2023-06-25 04:26:58 +0530

    Worked on edit profile page

--- src/pages/Dashboard/User/UserDash.jsx
+++ src/pages/Dashboard/User/UserDash.jsx
@@ -42,6 +42,8 @@ function UserDash() {
     setModal(false);
   };
 
+  const [editProfile, setEditProfile] = useState(true);
+
   const [upAppEmpty, setUpAppEmpty] = useState(false);
   const [recAppEmpty, setRecAppEmpty] = useState(false);
   const [presEmpty, setPresEmpty] = useState(false);
@@ -61,192 +63,220 @@ function UserDash() {
         )}
         <div className={styles.header}>
           <p className={styles.logotext}>MEDIC.</p>
-          <div className={styles.pfBox}>
-            <div className={styles.dropdown}>
-              <img
-                src={require("../../../assets/images/down.png")}
-                alt=""
-                draggable={false}
-                height={"18px"}
-              />
-              <div className={styles.dropdownContent}>
-                <Link href="/" style={{ textDecoration: "none" }}>
-                  Edit Profile
-                </Link>
-                <p onClick={toggleModal}>Edit Tag</p>
-                <a href="/" style={{ textDecoration: "none" }}>
-                  Logout
-                </a>
+          {!editProfile && (
+            <div className={styles.pfBox}>
+              <div className={styles.dropdown}>
+                <img
+                  src={require("../../../assets/images/down.png")}
+                  alt=""
+                  draggable={false}
+                  height={"18px"}
+                />
+                <div className={styles.dropdownContent}>
+                  <Link
+                    to="/dashboard/user/editprofile"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Edit Profile
+                  </Link>
+                  <p onClick={toggleModal}>Edit Tag</p>
+                  <a href="/" style={{ textDecoration: "none" }}>
+                    Logout
+                  </a>
+                </div>
               </div>
+              <Avatar
+                imgURL={require("../../../assets/images/pfptemplate.png")}
+                imgStyle={{ width: "50px", height: "50px" }}
+              />
             </div>
-            <Avatar
-              imgURL={require("../../../assets/images/pfptemplate.png")}
-              imgStyle={{ width: "50px", height: "50px" }}
-            />
-          </div>
+          )}
         </div>
-        <div className={styles.main}>
-          <div className={styles.mainHeader}>
-            <div className={styles.mainButtonContainer}>
-              <ButtonDark
-                text="Search for Doctors"
-                style={{ borderRadius: "100px" }}
-                ClickFunction={() => {
-                  Navigate("/search");
-                }}
-              />
-              <ButtonHollow
-                text="New Medical Card"
-                style={{ borderRadius: "100px" }}
-                ClickFunction={() => {
-                  Navigate("/view/medicalrecord");
-                }}
-              />
+        {!editProfile ? (
+          <div className={styles.main}>
+            <div className={styles.mainHeader}>
+              <div className={styles.mainButtonContainer}>
+                <ButtonDark
+                  text="Search for Doctors"
+                  style={{ borderRadius: "100px" }}
+                  ClickFunction={() => {
+                    Navigate("/search");
+                  }}
+                />
+                <ButtonHollow
+                  text="New Medical Card"
+                  style={{ borderRadius: "100px" }}
+                  ClickFunction={() => {
+                    Navigate("/view/medicalrecord");
+                  }}
+                />
+              </div>
+              <div className={styles.mainNameBox}>
+                {userInfo && (
+                  <p style={{ fontSize: "1.4rem" }}>{userInfo.name}</p>
+                )}
+              </div>
             </div>
-            <div className={styles.mainNameBox}>
-              {userInfo && (
-                <p style={{ fontSize: "1.4rem" }}>{userInfo.name}</p>
-              )}
+            <div className={styles.mainBody}>
+              <div className={styles.dashTop}>
+                <div className={styles.upApp}>
+                  <div className={styles.upAppContainer}>
+                    <div className={styles.dashDivHeader}>
+                      Upcoming Appointments
+                    </div>
+                    <div className={styles.dashDivContent}>
+                      {/* Use this div to map the upcoming appointments to */}
+                      {upAppEmpty ? (
+                        <>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                        </>
+                      ) : (
+                        <>
+                          <div className={styles.placeholderCont}>
+                            <p
+                              style={{
+                                textAlign: "center",
+                                fontSize: "1.5rem",
+                              }}
+                            >
+                              No Upcoming Appointments
+                            </p>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.recApp}>
+                  <div className={styles.recAppContainer}>
+                    <div className={styles.dashDivHeader}>
+                      Recent Appointments
+                    </div>
+                    <div className={styles.dashDivContent}>
+                      {/* Use this div to map the upcoming appointments to */}
+                      {recAppEmpty ? (
+                        <>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                        </>
+                      ) : (
+                        <>
+                          <div className={styles.placeholderCont}>
+                            <p
+                              style={{
+                                textAlign: "center",
+                                fontSize: "1.5rem",
+                              }}
+                            >
+                              No Recent Appointments
+                            </p>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.dashBottom}>
+                <div className={styles.MedicalRecords}>
+                  <div className={styles.MedicalRecordsContainer}>
+                    <div className={styles.dashDivHeader}>Medical Record</div>
+                    <div className={styles.dashDivContent}>
+                      {presEmpty ? (
+                        <>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                          <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
+                        </>
+                      ) : (
+                        <>
+                          <div className={styles.placeholderCont}>
+                            <p
+                              style={{
+                                textAlign: "center",
+                                fontSize: "1.5rem",
+                              }}
+                            >
+                              No Prescriptions Yet
+                            </p>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                <div className={styles.RecTransactions}>
+                  <div className={styles.RecTransactionsContainer}>
+                    <div className={styles.dashDivHeader}>
+                      Recent Transactions
+                    </div>
+                    <div className={styles.dashDivContent}>
+                      {recTransEmpty ? (
+                        <>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                          <Info textRight={"19/04/23"}>0.04ETH</Info>
+                        </>
+                      ) : (
+                        <>
+                          <div className={styles.placeholderCont}>
+                            <p
+                              style={{
+                                textAlign: "center",
+                                fontSize: "1.5rem",
+                              }}
+                            >
+                              No Transactions to show
+                            </p>
+                            <Button
+                              appearance="primary"
+                              endIcon={<Icon as={MetaMaskIcon} />}
+                            >
+                              Connect Wallet
+                            </Button>
+                          </div>
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className={styles.mainBody}>
-            <div className={styles.dashTop}>
-              <div className={styles.upApp}>
-                <div className={styles.upAppContainer}>
-                  <div className={styles.dashDivHeader}>
-                    Upcoming Appointments
-                  </div>
-                  <div className={styles.dashDivContent}>
-                    {/* Use this div to map the upcoming appointments to */}
-                    {upAppEmpty ? (
-                      <>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                      </>
-                    ) : (
-                      <>
-                        <div className={styles.placeholderCont}>
-                          <p
-                            style={{ textAlign: "center", fontSize: "1.5rem" }}
-                          >
-                            No Upcoming Appointments
-                          </p>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className={styles.recApp}>
-                <div className={styles.recAppContainer}>
-                  <div className={styles.dashDivHeader}>
-                    Recent Appointments
-                  </div>
-                  <div className={styles.dashDivContent}>
-                    {/* Use this div to map the upcoming appointments to */}
-                    {recAppEmpty ? (
-                      <>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                      </>
-                    ) : (
-                      <>
-                        <div className={styles.placeholderCont}>
-                          <p
-                            style={{ textAlign: "center", fontSize: "1.5rem" }}
-                          >
-                            No Recent Appointments
-                          </p>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className={styles.dashBottom}>
-              <div className={styles.MedicalRecords}>
-                <div className={styles.MedicalRecordsContainer}>
-                  <div className={styles.dashDivHeader}>Medical Record</div>
-                  <div className={styles.dashDivContent}>
-                    {presEmpty ? (
-                      <>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                        <Info textRight={"19/04/23"}>Dr. Rajesh Joshi</Info>
-                      </>
-                    ) : (
-                      <>
-                        <div className={styles.placeholderCont}>
-                          <p
-                            style={{ textAlign: "center", fontSize: "1.5rem" }}
-                          >
-                            No Prescriptions Yet
-                          </p>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                </div>
-              </div>
-              <div className={styles.RecTransactions}>
-                <div className={styles.RecTransactionsContainer}>
-                  <div className={styles.dashDivHeader}>
-                    Recent Transactions
-                  </div>
-                  <div className={styles.dashDivContent}>
-                    {recTransEmpty ? (
-                      <>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                        <Info textRight={"19/04/23"}>0.04ETH</Info>
-                      </>
-                    ) : (
-                      <>
-                        <div className={styles.placeholderCont}>
-                          <p
-                            style={{ textAlign: "center", fontSize: "1.5rem" }}
-                          >
-                            No Transactions to show
-                          </p>
-                          <Button
-                            appearance="primary"
-                            endIcon={<Icon as={MetaMaskIcon} />}
-                          >
-                            Connect Wallet
-                          </Button>
-                        </div>
-                      </>
-                    )}
-                  </div>
-                </div>
+        ) : (
+          <div className={styles.main_EditProfile}>
+            <div className={styles.mainHeader_EditProfile}><h1>Rahul</h1></div>
+            <div className={styles.mainBody_EditProfile}>
+              <div className={styles.dashTop_EditProfile}>
+                
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </>
   );
